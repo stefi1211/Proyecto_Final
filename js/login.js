5dfe77f7bb109e4812e0e5ac327d288f13345f2f
IsabellaGhiga <isabellaghiga@gmail.com>
2024-08-30 10:18:07 -0300

    actualizacion desafiate

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-document.getElementById('login-form').addEventListener('submit', function (e) {
+/*document.getElementById('login-form').addEventListener('submit', function (e) {
     e.preventDefault();
     const usuario = document.getElementById('usuario').value;
     const contraseña = document.getElementById('contraseña').value;
@@ -15,4 +15,26 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
     } else {
         alert('Usuario o contraseña incorrectos. Inténtalo de nuevo.');
     }
+});
+*/
+
+document.getElementById('login-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const usuario = document.getElementById('usuario').value;
+  const contraseña = document.getElementById('contraseña').value;
+
+  // Obtener los datos del localStorage
+  const storedUsuario = localStorage.getItem('usuario');
+  const storedContraseña = localStorage.getItem('contraseña');
+
+  if (usuario === storedUsuario && contraseña === storedContraseña) {
+    // Almacenar la sesión y el nombre de usuario en sessionStorage
+    sessionStorage.setItem('authenticated', 'true');
+    sessionStorage.setItem('username', usuario);
+
+    // Redirigir a la página de inicio
+    window.location.href = 'index.html';
+  } else {
+    alert('Usuario o contraseña incorrectos. Inténtalo de nuevo.');
+  }
 });
