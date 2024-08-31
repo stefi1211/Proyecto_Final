6b8dffde1fccaf52438e300018ae3862a78278db
Stefi <144570099+stefi1211@users.noreply.github.com>
2024-08-31 00:43:25 +0000

    saludar a usuario

--- js/updatenavbar.js
+++ js/updatenavbar.js
@@ -1,21 +1,20 @@
 // updateNavBar.js
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Obtener el nombre de usuario desde sessionStorage o localStorage
   const userName =
     sessionStorage.getItem('username') ||
     localStorage.getItem('username') ||
     null;
 
-  // Obtener el elemento de la barra de navegación
+ 
   const userLink = document.getElementById('user-link');
 
   if (userName) {
-    // Reemplazar el texto de 'Login' por el nombre del usuario y hacerlo un enlace al perfil
-    userLink.textContent = `${userName}`;
-    userLink.href = 'profile.html'; // Redirige al perfil del usuario o a otra página
+    
+    userLink.innerHTML = `<strong> Hola! </strong> ${userName}`;
+    userLink.href = 'profile.html'; 
   } else {
-    // Si no hay nombre de usuario, mostrar el enlace de login
+    
     userLink.textContent = 'Login';
     userLink.href = 'login.html';
   }
