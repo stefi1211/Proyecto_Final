1107d33e899ceeba748172ac2ad0feee25127c7a
Aguscampu <agustinacampu@gmail.com>
2024-09-01 00:04:13 -0300

    Revert "Merge branch 'main' of https://github.com/stefi1211/Proyecto_Final"
    This reverts commit de47b75457cf9674f803cb2010ed7091237425c9, reversing
    changes made to a62312b5978f8c860a4f5f51a41a8e2663ad15a1.

--- js/login.js
+++ js/login.js
@@ -55,25 +55,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-
-
-document.getElementById('login-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  const usuario = document.getElementById('usuario').value;
-  const contraseña = document.getElementById('contraseña').value;
-
-  // Obtener los datos del localStorage
-  const storedUsuario = localStorage.getItem('usuario');
-  const storedContraseña = localStorage.getItem('contraseña');
-
-  if (usuario === storedUsuario && contraseña === storedContraseña) {
-    // Almacenar la sesión y el nombre de usuario en sessionStorage
-    sessionStorage.setItem('authenticated', 'true');
-    sessionStorage.setItem('username', usuario);
-
-    // Redirigir a la página de inicio
-    window.location.href = 'index.html';
-  } else {
-    alert('Usuario o contraseña incorrectos. Inténtalo de nuevo.');
-  }
-});
