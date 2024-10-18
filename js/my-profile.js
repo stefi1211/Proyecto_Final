afb5dbc89593634cd5d1edae3d479f310a5c374e
Stefi <144570099+stefi1211@users.noreply.github.com>
2024-10-18 14:59:58 +0000

    cargar imagen del perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const authenticated = sessionStorage.getItem('authenticated');
 
@@ -11,11 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const emailField = document.getElementById('email');
   const profileForm = document.getElementById('profileForm');
+  const imageInput = document.getElementById('imageInput');
+  const profileImage = document.getElementById('profileImage');
+  const cameraIcon = document.querySelector('.camera-icon');
 
-  // cargar email en este caso usuario porque no se ingresa con email
+  // Cargar email en este caso usuario porque no se ingresa con email
   emailField.value = sessionStorage.getItem('username') || '';
 
-  // cargar otros datos
+  // Cargar otros datos
   const userData = JSON.parse(sessionStorage.getItem('userData')) || {};
   document.getElementById('firstName').value = userData.firstName || '';
   document.getElementById('secondName').value = userData.secondName || '';
@@ -23,10 +24,32 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('secondLastName').value = userData.secondLastName || '';
   document.getElementById('phone').value = userData.phone || '';
 
+  // cargar imagen del local
+  const profilePic = localStorage.getItem('profilePic');
+  if (profilePic) {
+      profileImage.src = profilePic;
+      profileImage.style.display = 'block'; 
+  }
+
+  // carga de imagen
+  imageInput.addEventListener('change', function (event) {
+      const file = event.target.files[0];
+      if (file) {
+          const reader = new FileReader();
+          reader.onload = function (e) {
+              profileImage.src = e.target.result;
+              profileImage.style.display = 'block';
+              cameraIcon.style.display = 'none'; 
+              localStorage.setItem('profilePic', e.target.result); 
+          };
+          reader.readAsDataURL(file);
+      }
+  });
+
   profileForm.addEventListener('submit', function (event) {
       event.preventDefault();
 
-      // campos obligatorios
+      // Campos obligatorios
       const firstName = document.getElementById('firstName').value.trim();
       const lastName = document.getElementById('lastName').value.trim();
       const email = document.getElementById('email').value.trim();
@@ -36,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
           return;
       }
 
-      // guardamos los datos
+      // Guardamos los datos
       const updatedUserData = {
           firstName,
           secondName: document.getElementById('secondName').value.trim(),
